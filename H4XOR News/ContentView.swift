1ee735b65a598b50403cd1379a4f44101cf702cc
michalrudy9 <michallrudy9@gmail.com>
2023-12-11 16:40:53 +0100

    Prepared data skeleton

--- H4XOR News/ContentView.swift
+++ H4XOR News/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(posts) { post in
+                Text(post.title)
+            }.navigationTitle("H4X0R NEWS")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Hallo"),
+    Post(id: "3", title: "Hola"),
+]
